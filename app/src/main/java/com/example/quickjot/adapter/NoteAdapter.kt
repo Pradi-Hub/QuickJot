b183d6eb989bd25f891deb7d1ad0b21eda4f94e6
Pradicksha <pradicksha1230@gmail.com>
2024-05-13 20:33:24 +0530

    note colors changed*

--- app/src/main/java/com/example/quickjot/adapter/NoteAdapter.kt
+++ app/src/main/java/com/example/quickjot/adapter/NoteAdapter.kt
@@ -2,15 +2,27 @@ package com.example.quickjot.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.example.quickjot.R
 import com.example.quickjot.databinding.NoteLayoutBinding
 import com.example.quickjot.fragments.HomeFragmentDirections
 import com.example.quickjot.model.Note
+import kotlin.random.Random
 
 class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    private val colorList = listOf(
+        R.color.c1,
+        R.color.c2,
+        R.color.c3,
+        R.color.c4,
+        R.color.c5,
+        R.color.c6
+    )
     class NoteViewHolder(val itemBinding: NoteLayoutBinding) : RecyclerView.ViewHolder(itemBinding.root)
 
     private val differCallback = object : DiffUtil.ItemCallback<Note>() {
@@ -35,11 +47,21 @@ class NoteAdapter : RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
         return  differ.currentList.size
     }
 
+    private fun randomColor(): Int {
+        val seed = System.currentTimeMillis().toInt()
+        val randomIndex = Random(seed).nextInt(colorList.size)
+        return colorList[randomIndex]
+    }
+
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
         val currentNote = differ.currentList[position]
 
         holder.itemBinding.noteTitle.text = currentNote.noteTitle
         holder.itemBinding.noteDesc.text = currentNote.noteDesc
+
+        holder.itemBinding.cardView.setCardBackgroundColor(
+            ContextCompat.getColor(holder.itemView.context, randomColor())
+        )
 
         holder.itemView.setOnClickListener {
             val direction = HomeFragmentDirections.actionHomeFragmentToEditNoteFragment(currentNote)
